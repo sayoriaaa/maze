e3de08a45abd90672f12b06166b300159f954c02
Li YiQing <73006417+sayoriaaa@users.noreply.github.com>
2022-03-16 15:39:17 +0800

    Add files via upload
    改成面向对象，增加了迷宫的求解(在递归生成迷宫的基础上改了一下)，能区分显示标准路径以及走入死胡同的路径。

--- gen_maze.py
+++ gen_maze.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Mar 16 12:31:16 2022
-
 @author: sayori
 """
 
@@ -17,9 +16,65 @@ def rmove():
     ret[pos]=choice
     return ret
 
+class Maze:
+    def __init__(self,width=10,height=10,method=0):
+        self.w=width
+        self.h=height
+        if method==0:
+            self.maze=gen1(width=self.w,height=self.h)
+        self.terminant=set_terminant(self.maze)
+        print(self.terminant)
+        
+    def solve_re(self):
+        def valid_move(i,j):
+            cnt=0
+            plane[i][j]=4
+            if i==self.terminant[0] and j==self.terminant[1]:
+                return True
+            while(cnt<1000):
+                cnt+=1
+                pos=rmove()
+                if i+pos[0]<0 or j+pos[1]<0 or i+pos[0]>2*width or j+pos[1]>2*height:
+                    continue
+                if plane[i+pos[0]][j+pos[1]]==8:
+                    if valid_move(i+pos[0],j+pos[1]):
+                        return True
+            plane[i][j]+=8#显示走进死胡同的路
+            return False
+        
+        plane=self.maze.copy()
+        width,height=self.w,self.h  
+        if not valid_move(1,1):
+            print('no solution')
+        else:
+            print('solution found')
+            self.solution=plane
+        
+        
+    def maze_show_path(self):
+        sns.heatmap(self.maze,cmap='GnBu')
+        plt.show()
+    
+    def maze_show_solution_path(self):
+        sns.heatmap(self.solution,cmap='GnBu')
+        plt.show()
+    
+    def maze_show_wall(self):
+        a=self.maze.copy()
+        w,h=a.shape
+        for i in range(1,w-1):
+            for j in range(1,h-1):
+                if a[i][j]==8:
+                    a[i][j]=0
+                else:a[i][j]=8
+        sns.heatmap(a,cmap='GnBu')
+        plt.show()
+        
+            
+
     
 
-def gen(width=10,height=10):
+def gen1(width=10,height=10):
     def valid_move(i,j):
         cnt=0
         plane[i][j]=8
@@ -37,20 +92,24 @@ def gen(width=10,height=10):
     for i in range(1,2*width):
         for j in range(1,2*height):
             if i%2==1 and j%2==1:
-                plane[i][j]=1 
-                
-    print(plane)
+                plane[i][j]=1   
     valid_move(1,1)
     return plane
 
-def maze_show_path(a):
-    sns.heatmap(a,cmap='GnBu')
-    plt.show()
+def set_terminant(a):
+    w,h=a.shape
+    for i in range(h-2,1,-1):
+        if a[w-2][i]==8:
+            return w-2,i
+        
+
+
+    
+    
     
     
 
 
-
-pa=gen(width=18,height=16)
-maze_show_path(pa)
-    
+a=Maze(width=10,height=10)
+a.solve_re()
+a.maze_show_solution_path()
